921ff7b569e3bd1a761d9605ce0b84391bc3be26
Adil Can Balcı <balciadilcan@gmail.com>
2024-01-07 00:27:57 +0300

    Isletim_Sistemleri_Vize
    KTUN - Yazılım Mühendisliği - İşletim Sistemleri Dersi Vize Ödevi

---
+++ İsletim_Sistemleri_Vize/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace İsletim_Sistemleri_Vize
+{
+    class Program
+    {
+        static void Main()
+        {
+            SayiIslemleri sayiIslemleri = new SayiIslemleri();
+            sayiIslemleri.SayilariOlustur(1, 1000000);
+
+            int parcaSayisi = 4;
+            List<List<int>> bolunmusListeler = sayiIslemleri.BolunmusListeleriOlustur(parcaSayisi);
+
+            Thread t1 = new Thread(() => sayiIslemleri.CiftSayilariBul(bolunmusListeler));
+            Thread t2 = new Thread(() => sayiIslemleri.TekSayilariBul(bolunmusListeler));
+            Thread t3 = new Thread(() => sayiIslemleri.AsalSayilariBul(bolunmusListeler));
+            Thread t4 = new Thread(() => sayiIslemleri.AsalSayilariBul(bolunmusListeler));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+
+            sayiIslemleri.SonuclariYazdir();
+        }
+
+    }
+}
---
+++ İsletim_Sistemleri_Vize/SayiIslemleri.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İsletim_Sistemleri_Vize
+{
+    internal class SayiIslemleri
+    {
+        private List<int> sayilar = new List<int>();
+        private List<int> ciftSayilar = new List<int>();
+        private List<int> tekSayilar = new List<int>();
+        private List<int> asalSayilar = new List<int>();
+        private object lockObj = new object();
+
+        public void SayilariOlustur(int baslangic, int bitis)
+        {
+            for (int i = baslangic; i <= bitis; i++)
+            {
+                sayilar.Add(i);
+            }
+        }
+
+        public List<List<int>> BolunmusListeleriOlustur(int parcaSayisi)
+        {
+            int parcaUzunlugu = sayilar.Count / parcaSayisi;
+            List<List<int>> bolunmusListeler = new List<List<int>>();
+
+            for (int i = 0; i < parcaSayisi; i++)
+            {
+                List<int> parca = sayilar.Skip(i * parcaUzunlugu).Take(parcaUzunlugu).ToList();
+                bolunmusListeler.Add(parca);
+            }
+
+            return bolunmusListeler;
+        }
+
+        public void CiftSayilariBul(List<List<int>> parcaliListeler)
+        {
+            lock (lockObj)
+            {
+                foreach (var parca in parcaliListeler)
+                {
+                    foreach (int sayi in parca)
+                    {
+                        if (sayi % 2 == 0)
+                        {
+                            ciftSayilar.Add(sayi);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void TekSayilariBul(List<List<int>> parcaliListeler)
+        {
+            lock (lockObj)
+            {
+                foreach (var parca in parcaliListeler)
+                {
+                    foreach (int sayi in parca)
+                    {
+                        if (sayi % 2 != 0)
+                        {
+                            tekSayilar.Add(sayi);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void AsalSayilariBul(List<List<int>> parcaliListeler)
+        {
+            foreach (var parca in parcaliListeler)
+            {
+                foreach (int sayi in parca)
+                {
+                    int sayac = 0;
+                    for (int i = 2; i <= Math.Sqrt(sayi); i++)
+                    {
+                        if (sayi % i == 0)
+                        {
+                            sayac++;
+                            break;
+                        }
+                    }
+
+                    if (sayac == 0 && sayi > 1)
+                    {
+                        lock (lockObj)
+                        {
+                            if (!asalSayilar.Contains(sayi))
+                            {
+                                asalSayilar.Add(sayi);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void SonuclariYazdir()
+        {
+            Console.WriteLine("Tek Sayilar: " + string.Join(", ", tekSayilar.Distinct()));
+            Console.WriteLine();
+            Console.WriteLine("Cift Sayilar: " + string.Join(", ", ciftSayilar.Distinct()));
+            Console.WriteLine();
+            Console.WriteLine("Asal Sayilar: " + string.Join(", ", asalSayilar.Distinct()));
+        }
+    }
+}
